85e2928602a8666772853c355a125625e88288ba
AlejandroSuarez7 <alejandro.suarez0321@gmail.com>
2025-09-05 06:24:04 -0500

    Visual

--- src/features/dashboard/pages/DashboardPage.jsx
+++ src/features/dashboard/pages/DashboardPage.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import './stylesDash.css'
 export function DashboardPage() {
   return (
-    <div style={{ textAlign: 'center' }}>
+    <div>
       <h1>Dashboard Principal</h1>
       <p>Bienvenido a la implementación de referencia de la Tienda Online.</p>
     </div>
--- src/features/products/pages/ProductsPage.jsx
+++ src/features/products/pages/ProductsPage.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import ProductList from './ProductsList';
 export function ProductsPage() {
   return (
     <div>
       <h1 style={{ textAlign: 'center' }}>Catálogo de Productos</h1>
       <p style={{ textAlign: 'center' }}>
         Aquí se mostrará la lista de productos de la tienda.
+        <ProductList />
       </p>
     </div>
   );
